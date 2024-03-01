714d74b6c6ae0194573a02d9e2a9805ed74c7ad4
Joao Barcante <joao.barcante@somalogistica.com.br>
2024-03-01 10:53:17 -0300

    Cria mapeamento para usuário

---
+++ EleicaoCipa/Data/Configurations/ConfigurationUsuario.cs
@@ -0,0 +1,19 @@
+﻿using EleicaoCipa.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EleicaoCipa.Data.Configurations;
+
+public class ConfigurationUsuario : IEntityTypeConfiguration<Usuario>
+{
+    public void Configure(EntityTypeBuilder<Usuario> builder)
+    {
+        builder
+            .HasKey(usuario => usuario.Id);
+
+        builder
+            .Property(usuario => usuario.Nome)
+            .HasColumnType("varchar(100)")
+            .IsRequired();
+    }
+}
