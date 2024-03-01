5c6ebf8b1efae3d732603c88bf0f4422538899fb
Joao Barcante <joao.barcante@somalogistica.com.br>
2024-03-01 12:41:44 -0300

    Configura AutoMapper

--- EleicaoCipa/Program.cs
+++ EleicaoCipa/Program.cs
@@ -7,6 +7,9 @@ var connectionString = builder.Configuration.GetConnectionString("EleicaoConnect
 builder.Services.AddDbContext<EleicaoContext>(opts => 
     opts.UseLazyLoadingProxies().UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
+builder.Services.
+    AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 // Add services to the container.
 
 builder.Services.AddControllers();
