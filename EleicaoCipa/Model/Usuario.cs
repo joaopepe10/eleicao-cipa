805029d0aa326c2ff88cbdba5647ae6f37ad3c53
Joao Barcante <joao.barcante@somalogistica.com.br>
2024-03-01 08:37:58 -0300

    Cria entidade Usuario

---
+++ EleicaoCipa/Model/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EleicaoCipa.Model;
+
+public class Usuario
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+    [Required(ErrorMessage = "Nome é obrigatório")]
+    public string Nome { get; set; }
+
+
+}
