13509e849621f99ccde4d82a8b1451eaa9917c15
Joao Barcante <joao.barcante@somalogistica.com.br>
2024-03-01 08:54:25 -0300

    Cria entidade Voto

---
+++ EleicaoCipa/Model/Voto.cs
@@ -0,0 +1,10 @@
+﻿namespace EleicaoCipa.Model;
+
+public class Voto
+{
+    public int Id { get; set; }
+    public int EleicaoId { get; set; }
+    public int EleitorId { get; set; }
+    public int CandidatoId { get; set; }
+    public DateTime DataVoto { get; set; }
+}
