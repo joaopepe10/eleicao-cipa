c112dd7dfacaa74fa7ff0713e55454ea6ed003de
Joao Barcante <joao.barcante@somalogistica.com.br>
2024-03-01 13:55:53 -0300

    Relaciona eleição e candidado

---
+++ EleicaoCipa/Data/Configurations/ConfigurationEleicao.cs
@@ -0,0 +1,34 @@
+﻿using EleicaoCipa.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EleicaoCipa.Data.Configurations;
+
+public class ConfigurationEleicao : IEntityTypeConfiguration<Eleicao>
+{
+    public void Configure(EntityTypeBuilder<Eleicao> builder)
+    {
+        builder
+            .HasKey(eleicao => eleicao.Id);
+
+        builder
+            .Property(eleicao => eleicao.DataInicio)
+            .HasColumnType("datetime")
+            .IsRequired();
+
+        builder
+            .Property(eleicao => eleicao.DataFim)
+            .HasColumnType("datetime")
+            .IsRequired();
+
+        builder
+            .Property(eleicao => eleicao.Status)
+            .HasColumnType("tinyint")
+            .IsRequired();
+
+        builder
+            .HasMany(eleicao => eleicao.Candidatos)
+            .WithOne(candidato => candidato.Eleicao)
+            .HasForeignKey(eleicao => eleicao.EleicaoId);
+    }
+}
--- EleicaoCipa/Model/Eleicao.cs
+++ EleicaoCipa/Model/Eleicao.cs
@@ -7,5 +7,6 @@ public class Eleicao
     public int Id { get; set; }
     public DateTime DataInicio { get; set; }
     public DateTime DataFim { get; set; }
-    public StatusEnum Status {  get; set; } 
+    public StatusEnum Status {  get; set; }
+    public virtual ICollection<Candidato> Candidatos { get; set; }
 }
