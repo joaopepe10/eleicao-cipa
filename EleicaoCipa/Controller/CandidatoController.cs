9a206ed1fc1e056a2c8e18bc77ff77166bb57e54
Joao Barcante <joao.barcante@somalogistica.com.br>
2024-03-01 13:36:12 -0300

    Cria controllers e mapeia rotas

---
+++ EleicaoCipa/Controller/CandidatoController.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using EleicaoCipa.Data.Dto.UsuarioDto;
+using EleicaoCipa.Data;
+using EleicaoCipa.Model;
+using Microsoft.AspNetCore.Mvc;
+using EleicaoCipa.Data.Dto.CandidatoDto;
+
+namespace EleicaoCipa.Controller;
+[ApiController]
+[Route("[controller]")]
+public class CandidatoController : ControllerBase
+{
+    private EleicaoContext _context;
+    private IMapper _mapper;
+
+    public CandidatoController(EleicaoContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    [HttpPost]
+    public IActionResult Post([FromBody] CreateCandidatoDto dto)
+    {
+        var candidato = _mapper.Map<Candidato>(dto);
+        var readCandidato = _mapper.Map<ReadCandidatoDto>(candidato);
+        _context.Candidatos.Add(candidato);
+        _context.SaveChanges();
+        return CreatedAtAction(nameof(GetById), new { Id = candidato.Id }, readCandidato);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+        var candidato = _context.Candidatos.FirstOrDefault(candidato => candidato.Id == id);
+        if (candidato == null) return NotFound();
+
+        var dto = _mapper.Map<ReadCandidatoDto>(candidato);
+        return Ok(dto);
+    }
+}
---
+++ EleicaoCipa/Controller/UsuarioController.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using EleicaoCipa.Data;
+using EleicaoCipa.Data.Dto.UsuarioDto;
+using EleicaoCipa.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EleicaoCipa.Controller;
+[ApiController]
+[Route("[controller]")]
+public class UsuarioController : ControllerBase
+{
+    private EleicaoContext _context;
+    private IMapper _mapper;
+
+    public UsuarioController(EleicaoContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    [HttpPost]
+    public IActionResult Post([FromBody] CreateUsuarioDto dto)
+    {
+        var usuario = _mapper.Map<Usuario>(dto);
+        var readUsuario = _mapper.Map<ReadUsuarioDto>(usuario);
+        _context.Usuarios.Add(usuario);
+        _context.SaveChanges();
+        return CreatedAtAction(nameof(GetById), new { Id = usuario.Id }, readUsuario);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+        var usuario = _context.Usuarios.FirstOrDefault(usuario => usuario.Id == id);
+        if (usuario == null) return NotFound();
+
+        var dto = _mapper.Map<ReadUsuarioDto>(usuario);
+        return Ok(dto);
+    }
+
+    [HttpGet("candidatos")]
+    public IEnumerable<ReadAllCandidatosDto> GetAllCandidatos()
+    {
+        var candidatos = _mapper.Map<List<ReadAllCandidatosDto>>(_context.Usuarios.ToList());
+
+        return candidatos;
+    }
+}
