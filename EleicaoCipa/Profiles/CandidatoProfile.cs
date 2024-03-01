35ff957855b6546c99b7b4b93722cbaab108e14e
Joao Barcante <joao.barcante@somalogistica.com.br>
2024-03-01 12:48:22 -0300

    Cria mapeamento Dto para Entidade

---
+++ EleicaoCipa/Profiles/CandidatoProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using EleicaoCipa.Data.Dto.CandidatoDto;
+using EleicaoCipa.Model;
+
+namespace EleicaoCipa.Profiles;
+
+public class CandidatoProfile : Profile
+{
+    public CandidatoProfile()
+    {
+        CreateMap<CreateCandidatoDto, Candidato>();
+        CreateMap<Candidato, ReadCandidatoDto>();
+    }
+}
---
+++ EleicaoCipa/Profiles/UsuarioProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using EleicaoCipa.Data.Dto.UsuarioDto;
+using EleicaoCipa.Model;
+
+namespace EleicaoCipa.Profiles;
+
+public class UsuarioProfile : Profile
+{
+    public UsuarioProfile()
+    {
+        CreateMap<CreateUsuarioDto, Usuario>();
+        CreateMap<Usuario, ReadUsuarioDto>();
+        CreateMap<UpdateUsuarioDto, Usuario>();
+    }
+}
