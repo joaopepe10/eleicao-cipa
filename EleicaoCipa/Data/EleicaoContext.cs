df881f9ef8f186fecfbbcd32fa074a4316cfcfb1
Joao Barcante <joao.barcante@somalogistica.com.br>
2024-03-01 10:57:18 -0300

    Define configuração para todas as entidades mapeadas

---
+++ EleicaoCipa/Data/EleicaoContext.cs
@@ -0,0 +1,19 @@
+﻿using EleicaoCipa.Data.Configurations;
+using EleicaoCipa.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace EleicaoCipa.Data;
+
+public class EleicaoContext : DbContext
+{
+    public EleicaoContext(DbContextOptions<EleicaoContext> opts) : base(opts) { }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+    }
+    public DbSet<Usuario> Usuarios { get; set; }
+}
