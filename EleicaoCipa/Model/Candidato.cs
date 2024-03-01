b3461a5e222a045414427ec466faf54d166fe41a
Joao Barcante <joao.barcante@somalogistica.com.br>
2024-03-01 08:43:18 -0300

    Cria entidade Candidato

---
+++ EleicaoCipa/Model/Candidato.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EleicaoCipa.Model;
+
+public class Candidato
+{    
+    public int Id { get; set; }
+    public int UsuarioId { get; set; }
+    public int EleicaoId { get; set; }    
+    public string Discurso { get; set; }
+}
