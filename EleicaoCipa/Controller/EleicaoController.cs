d8f337e11a56bace9cb6f13dc0d42ce17de1a4c5
Joao Barcante <joao.barcante@somalogistica.com.br>
2024-03-06 13:49:55 -0300

    Altera endo-point

--- EleicaoCipa/Controller/EleicaoController.cs
+++ EleicaoCipa/Controller/EleicaoController.cs
@@ -23,21 +23,7 @@ public class EleicaoController : ControllerBase
         var responseDto = _service.Post(dto);
         return CreatedAtAction(nameof(GetById), new { Id = responseDto.Id }, responseDto);
     }
-
-    [HttpPost("insere_candidato")]
-    public IActionResult PostCondidato([FromBody] CreateCandidatoDto dto)
-    {
-        try
-        {
-            var resposnseCandidatoDto = _service.PostCandidato(dto);
-            return Ok(resposnseCandidatoDto);
-        }
-         catch (Exception ex)
-        {
-            return NotFound(ex.Message);
-        }
-    }
-
+    
     [HttpGet("{id}")]
     public IActionResult GetById(int id)
     {
@@ -46,34 +32,23 @@ public class EleicaoController : ControllerBase
         return Ok(dto);
     }
 
+    [HttpPost("{id}/insere_candidato")]
+    public IActionResult PostCondidato(int id, [FromBody] CreateCandidatoDto dto)
+    {
+        try
+        {
+            var responseCandidato = _service.PostCandidato(id, dto);
+            return Ok(responseCandidato);
+        }
+         catch (Exception ex)
+        {
+            return NotFound(ex.Message);
+        }
+    }
+
     [HttpGet]
     public IEnumerable<ReadEleicaoDto> GetAll()
     {
         return _service.GetAll();
     }
-
-    [HttpPut("status/{id}")]
-    public IActionResult UpdateStatus(int id, [FromBody] UpadateStatusEleicaoDto dto)
-    {
-        
-        return NoContent();
-    }
-    //// COLOCAR VALIDAÇÃO DE INSERÇÃO DE DATA, TEMPO MINIMO
-    //// COLOCAR VALIDAÇÃO DE QUE DATA-INICIO N PODE SER MAIOR QUE DATA-FIM E AO CONTRÁRIO
-    //[HttpPut("data-inicio/{id}")]
-    //public IActionResult UpdateDataInicio(int id, [FromBody] UpdateEleicaoDataInicioDto dto)
-    //{
-    //    var eleicao = _context.Eleicoes.Find(id);
-    //    if (eleicao == null) return NotFound();
-    //    _mapper.Map(dto, eleicao);
-    //    _context.SaveChanges();
-    //    return NoContent();
-    //}
-
-    [HttpPut("data-fim/{id}")]
-    public IActionResult UpdateDataFim(int id, [FromBody] CreateEleicaoDto dto)
-    {        
-        return Ok(_service.Update(dto, id));
-    }
-    
 }
