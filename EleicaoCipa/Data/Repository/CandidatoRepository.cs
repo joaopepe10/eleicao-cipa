a844947df47fed8f1d0c4949feff80430ff66a38
Joao Barcante <joao.barcante@somalogistica.com.br>
2024-03-06 09:59:43 -0300

    Cria repositório para Candidato

---
+++ EleicaoCipa/Data/Repository/CandidatoRepository.cs
@@ -0,0 +1,38 @@
+﻿using EleicaoCipa.Data.Repository.Interface;
+using EleicaoCipa.Model;
+
+namespace EleicaoCipa.Data.Repository;
+
+public class CandidatoRepository : ICandidatoRepository
+{
+    private EleicaoContext _context;
+    public CandidatoRepository(EleicaoContext context)
+    {
+
+        _context = context;
+    }
+    public Candidato Post(Candidato candidato)
+    {
+        _context.Candidatos.Add(candidato);
+        _context.SaveChanges();
+        return candidato;
+    }
+
+    public IEnumerable<Candidato> GetAll()
+    {
+        return _context.Candidatos.ToList();
+    }
+
+    public Candidato? GetById(int id)
+    {
+        return _context.Candidatos.FirstOrDefault(candidato => candidato.Id == id);
+    }
+
+
+    public Candidato Update(Candidato candidato)
+    {
+        _context.Update(candidato);
+        _context.SaveChanges();
+        return candidato;
+    }
+}
