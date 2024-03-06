1eafe4a7467841e5d8cf98835d8f48761f6b71f5
Joao Barcante <joao.barcante@somalogistica.com.br>
2024-03-06 09:59:24 -0300

    Cria interface para repositório de Candidato

---
+++ EleicaoCipa/Data/Repository/Interface/ICandidatoRepository.cs
@@ -0,0 +1,11 @@
+﻿using EleicaoCipa.Model;
+
+namespace EleicaoCipa.Data.Repository.Interface;
+
+public interface ICandidatoRepository
+{
+    Candidato Post(Candidato candidato);
+    Candidato? GetById(int id);
+    IEnumerable<Candidato> GetAll();
+    Candidato Update(Candidato candidato);
+}
