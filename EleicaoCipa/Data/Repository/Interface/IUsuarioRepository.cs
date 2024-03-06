6809ed60fffa1a759df5503f586b8b001bd04e0b
João Perdro Pires <joaopedropires61@gmail.com>
2024-03-06 17:53:21 -0300

    Cria camada de repositório

---
+++ EleicaoCipa/Data/Repository/Interface/IUsuarioRepository.cs
@@ -0,0 +1,11 @@
+﻿using EleicaoCipa.Model;
+
+namespace EleicaoCipa.Data.Repository.Interface;
+
+public interface IUsuarioRepository
+{
+    Usuario Post(Usuario usuario);
+    Usuario? GetById(int id);
+    IEnumerable<Usuario> GetAll();
+    Usuario Update(Usuario candidato);
+}
---
+++ EleicaoCipa/Data/Repository/UsuarioRepository.cs
@@ -0,0 +1,38 @@
+﻿using EleicaoCipa.Data.Repository.Interface;
+using EleicaoCipa.Model;
+
+namespace EleicaoCipa.Data.Repository;
+
+public class UsuarioRepository : IUsuarioRepository
+{
+    private readonly EleicaoContext _context;
+
+    public UsuarioRepository(EleicaoContext context)
+    {
+        _context = context;
+    }
+
+    public Usuario Post(Usuario usuario)
+    {
+        _context.Usuarios.Add(usuario);
+        _context.SaveChanges();
+        return usuario;
+    }
+
+    public IEnumerable<Usuario> GetAll()
+    {
+        return _context.Usuarios.ToList();
+    }
+
+    public Usuario? GetById(int id)
+    {
+        return _context.Usuarios.FirstOrDefault(usuario => usuario.Id == id);
+    }
+
+    public Usuario Update(Usuario usuario)
+    {
+        _context.Usuarios.Update(usuario);
+        _context.SaveChanges();
+        return usuario;
+    }
+}
