17b17adc7e84cc6b7e5389efed082c282097e7ef
Joao Barcante <joao.barcante@somalogistica.com.br>
2024-03-07 13:49:38 -0300

    Cria end-poins para Voto

---
+++ EleicaoCipa/Controller/VotoController.cs
@@ -0,0 +1,36 @@
+﻿using EleicaoCipa.ApplicationService;
+using EleicaoCipa.Data.Dto.VotoDto.RequestDto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EleicaoCipa.Controller;
+[ApiController]
+[Route("[controller]")]
+public class VotoController : ControllerBase
+{
+    private readonly ApplicationServiceVoto _service;
+
+    public VotoController(ApplicationServiceVoto service)
+    {
+        _service = service;
+    }
+
+    [HttpPost]
+    public IActionResult Post(CreateVotoDto dto)
+    {
+        if (dto == null) return NotFound();
+        var responseDto = _service.Post(dto);
+        return CreatedAtAction(nameof(GetById), new { Id = responseDto.Id }, responseDto);
+    }
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+        try
+        {
+            var responseDto = _service.GetById(id);
+            return Ok(responseDto);
+        }catch (Exception ex)
+        {
+            return NotFound(ex.Message);
+        }
+    }
+}
