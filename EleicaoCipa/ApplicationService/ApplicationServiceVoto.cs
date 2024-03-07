10d012267d1385da6b90a698528e3b75c5461d58
Joao Barcante <joao.barcante@somalogistica.com.br>
2024-03-07 13:35:50 -0300

    Cria camada de Service para Voto

---
+++ EleicaoCipa/ApplicationService/ApplicationServiceVoto.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using EleicaoCipa.Data.Dto.VotoDto;
+using EleicaoCipa.Data.Dto.VotoDto.RequestDto;
+using EleicaoCipa.Data.Repository;
+using EleicaoCipa.Model;
+
+namespace EleicaoCipa.ApplicationService;
+
+public class ApplicationServiceVoto
+{
+    private readonly VotoRepository _repository;
+    private readonly IMapper _mapper;
+
+    public ApplicationServiceVoto(VotoRepository repository,IMapper mapper)
+    {
+        _repository = repository;
+        _mapper = mapper;
+    }
+
+    public ReadVotoDto Post(CreateVotoDto dto)
+    {
+        var entity = _mapper.Map<Voto>(dto);
+        _repository.Post(entity);
+        return _mapper.Map<ReadVotoDto>(entity);
+    }
+
+    public IEnumerable<ReadVotoDto> GetAll()
+    {
+        var votos = _repository.GetAll();
+        return _mapper.Map<List<ReadVotoDto>>(votos);
+    }
+
+}
