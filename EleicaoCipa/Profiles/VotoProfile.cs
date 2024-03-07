df8e6eb777ec3ea651c6b51f5825efa20f6fefb6
Joao Barcante <joao.barcante@somalogistica.com.br>
2024-03-07 13:37:27 -0300

    Cria Profile do AutoMapper para Voto

---
+++ EleicaoCipa/Profiles/VotoProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using EleicaoCipa.Data.Dto.VotoDto;
+using EleicaoCipa.Data.Dto.VotoDto.RequestDto;
+using EleicaoCipa.Model;
+
+namespace EleicaoCipa.Profiles;
+
+public class VotoProfile : Profile
+{
+    public VotoProfile()
+    {
+        CreateMap<CreateVotoDto, Voto>().ReverseMap();
+        CreateMap<Voto, ReadVotoDto>().ReverseMap();
+    }
+}
