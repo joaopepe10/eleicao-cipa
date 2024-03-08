fc61487c37eff899fdedfe705b8006f74de0a968
Joao Barcante <joao.barcante@somalogistica.com.br>
2024-03-08 13:19:15 -0300

    Criação de dtos para retorno de resultado

---
+++ EleicaoCipa/Data/Dto/CandidatoDto/ResponseDto/ReadCandidatoVotosDto.cs
@@ -0,0 +1,7 @@
+﻿namespace EleicaoCipa.Data.Dto.CandidatoDto.ResponseDto;
+
+public class ReadCandidatoVotosDto : BaseDto
+{
+    public string Nome { get; set; }
+    public int QuantidadeDeVotos { get; set; }
+}
---
+++ EleicaoCipa/Data/Dto/EleicaoDto/ResponseDto/ReadResultadoEleicaoDto.cs
@@ -0,0 +1,9 @@
+﻿using EleicaoCipa.Data.Dto.CandidatoDto.ResponseDto;
+
+namespace EleicaoCipa.Data.Dto.EleicaoDto.ResponseDto;
+
+public class ReadResultadoEleicaoDto 
+{
+    public int EleicaoId { get; set; }  
+    public IEnumerable<ReadCandidatoVotosDto> Candidatos { get; set; }
+}
