92ac92b5620b34e8e4a49577e66fe241593b6d92
Joao Barcante <joao.barcante@somalogistica.com.br>
2024-03-08 12:39:48 -0300

    Adciona propriedade para retorno

--- EleicaoCipa/Data/Dto/CandidatoDto/ResponseDto/ReadCandidatoDto.cs
+++ EleicaoCipa/Data/Dto/CandidatoDto/ResponseDto/ReadCandidatoDto.cs
@@ -7,6 +7,7 @@ public class ReadCandidatoDto : BaseDto
     public int UsuarioId { get; set; }
     public string NomeCandidato { get; set; }
     public string Discurso { get; set; }
+    public int EleicaoId { get; set; }
     public DateTime DataInscricao { get; set; }
     public int QuantidadeDeVotos { get; set; }
 }
