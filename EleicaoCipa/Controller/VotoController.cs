683b11fd24cd7566c1c42a9d74550f94b6a50526
Joao Barcante <joao.barcante@somalogistica.com.br>
2024-03-08 11:02:28 -0300

    Cria end-point para retorno de ID na crição de entidade

--- EleicaoCipa/Controller/VotoController.cs
+++ EleicaoCipa/Controller/VotoController.cs
@@ -22,4 +22,18 @@ public class VotoController : ControllerBase
         var responseDto = _service.Post(dto);
         return CreatedAtAction(nameof(GetById), new { Id = responseDto.Id }, responseDto);
     }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+        try
+        {
+            var responseDto = _service.GetById(id);
+            return Ok(responseDto);
+        }
+        catch (Exception ex)
+        {
+            return NotFound(ex.Message);
+        }
+    }
 }
