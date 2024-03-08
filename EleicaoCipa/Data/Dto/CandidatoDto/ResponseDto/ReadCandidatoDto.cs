b61773672b76ef26834c32d164761009e4ea1f0d
Joao Barcante <joao.barcante@somalogistica.com.br>
2024-03-08 11:03:02 -0300

    Altera retorno de objeto para propriedade em ReadCandidatoDto

--- EleicaoCipa/Data/Dto/CandidatoDto/ResponseDto/ReadCandidatoDto.cs
+++ EleicaoCipa/Data/Dto/CandidatoDto/ResponseDto/ReadCandidatoDto.cs
@@ -5,7 +5,7 @@ namespace EleicaoCipa.Data.Dto.CandidatoDto.ResponseDto;
 public class ReadCandidatoDto : BaseDto
 {
     public int UsuarioId { get; set; }
-    public ReadUsuarioDto Usuario { get; set; }
+    public string NomeCandidato { get; set; }
     public string Discurso { get; set; }
     public DateTime DataInscricao { get; set; }
     public int QuantidadeDeVotos { get; set; }
