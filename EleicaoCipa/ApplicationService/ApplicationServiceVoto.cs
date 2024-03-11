516bab2253be5b67d0a995a3afe6011404f8fd28
Joao Barcante <joao.barcante@somalogistica.com.br>
2024-03-11 11:05:36 -0300

    Cria método para validações de voto

--- EleicaoCipa/ApplicationService/ApplicationServiceVoto.cs
+++ EleicaoCipa/ApplicationService/ApplicationServiceVoto.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Castle.Core.Internal;
 using EleicaoCipa.Data.Dto.VotoDto;
 using EleicaoCipa.Data.Dto.VotoDto.RequestDto;
 using EleicaoCipa.Data.Repository;
@@ -10,17 +11,18 @@ public class ApplicationServiceVoto
 {
     private readonly VotoRepository _repository;
     private readonly IMapper _mapper;
+    private readonly ApplicationServiceEleicao _serviceEleicao;
 
-    public ApplicationServiceVoto(VotoRepository repository,IMapper mapper)
+    public ApplicationServiceVoto(VotoRepository repository,IMapper mapper, ApplicationServiceEleicao serviceEleicao)
     {
         _repository = repository;
         _mapper = mapper;
+        _serviceEleicao = serviceEleicao;
     }
 
     public ReadVotoDto Post(CreateVotoDto dto)
     {
-        if (ExistsVotoDuplicado(dto.UsuarioId, dto.EleicaoId))
-            throw new Exception("Não foi possível registrar este voto.");
+        ValidaVoto(dto);
         var entity = _mapper.Map<Voto>(dto);
         _repository.Post(entity);
         return _mapper.Map<ReadVotoDto>(entity);
@@ -40,5 +42,14 @@ public class ApplicationServiceVoto
         var votos = _repository.GetAll();
         return votos
             .Any(voto => voto.EleicaoId == eleicaoId && voto.UsuarioId == usuarioId);
+    }
+
+    private void ValidaVoto(CreateVotoDto dto)
+    {
+        var eleicao = _serviceEleicao.GetEleicaoById(dto.EleicaoId);
+        if (eleicao.Candidatos.IsNullOrEmpty())
+            throw new Exception($"Eleição com ID {dto.EleicaoId} está vazia, não é possível votar.");
+        if (ExistsVotoDuplicado(dto.UsuarioId, dto.EleicaoId))
+            throw new Exception("Não foi possível registrar este voto pois está duplicado.");
     }
 }
