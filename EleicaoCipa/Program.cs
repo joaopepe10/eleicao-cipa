7d3ef764a3666a0534bdd543d9243c3f01f1a891
Joao Barcante <joao.barcante@somalogistica.com.br>
2024-03-13 12:56:08 -0300

    Altera referência de namespace em Program

--- EleicaoCipa/Program.cs
+++ EleicaoCipa/Program.cs
@@ -1,6 +1,6 @@
-using EleicaoCipa.ApplicationService;
-using EleicaoCipa.Data;
-using EleicaoCipa.Data.Repository;
+using EleicaoCipaVotacao.ApplicationService;
+using EleicaoCipaVotacao.Data;
+using EleicaoCipaVotacao.Data.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
