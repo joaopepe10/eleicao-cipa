3804da915f180a6599e2cc198cb581154599c561
Joao Barcante <joao.barcante@somalogistica.com.br>
2024-03-13 12:45:23 -0300

    Altera namespace e altera de class para interface

--- Domain.Core/Interfaces/Services/IServiceCandidato.cs
+++ Domain.Core/Interfaces/Services/IServiceCandidato.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using EleicaoCipa.Domain.Model;
 
-namespace Domain.Core.Interfaces.Services;
+namespace EleicaoCipa.Domain.Core.Interfaces.Services;
 
-public class IServiceCandidato : IServiceBase
+public interface IServiceCandidato : IServiceBase<Candidato>
 {
 }
--- Domain.Core/Interfaces/Services/IServiceUsuario.cs
+++ Domain.Core/Interfaces/Services/IServiceUsuario.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using EleicaoCipa.Domain.Model;
 
-namespace Domain.Core.Interfaces.Services;
+namespace EleicaoCipa.Domain.Core.Interfaces.Services;
 
-public class IServiceUsuario : IServiceBase
+public interface IServiceUsuario : IServiceBase<Usuario>
 {
 }
