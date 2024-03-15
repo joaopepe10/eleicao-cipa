7efe0d35385a974ecac91b8c7344af28ddc2f9fe
Joao Barcante <joao.barcante@somalogistica.com.br>
2024-03-15 09:47:06 -0300

    Adciona Mappings

---
+++ EleicaoCipa.Infrastructure.Repository/Data/Mappings/CandidatoMapping.cs
@@ -0,0 +1,30 @@
+﻿using EleicaoCipa.Dominio.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EleicaoCipaVotacao.Data.Mappings;
+
+public class CandidatoMapping : IEntityTypeConfiguration<Candidato>
+{
+    public void Configure(EntityTypeBuilder<Candidato> builder)
+    {
+        builder
+            .HasKey(candidato => candidato.Id);
+
+        builder.
+            Property(candidato => candidato.DataInscricao)
+            .HasColumnName("data_inscricao")
+            .HasColumnType("datetime")
+            .IsRequired();
+
+        builder
+            .Property(candidato => candidato.Discurso)
+            .HasColumnType("text")
+            .IsRequired();
+
+        builder
+            .HasOne(candidato => candidato.Usuario)
+            .WithMany(usuario => usuario.Candidatos)
+            .HasForeignKey(candidato => candidato.UsuarioId);
+    }
+}
---
+++ EleicaoCipa.Infrastructure.Repository/Data/Mappings/UsuarioMapping.cs
@@ -0,0 +1,19 @@
+﻿using EleicaoCipa.Dominio.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EleicaoCipaVotacao.Data.Mappings;
+
+public class UsuarioMapping : IEntityTypeConfiguration<Usuario>
+{
+    public void Configure(EntityTypeBuilder<Usuario> builder)
+    {
+        builder
+            .HasKey(usuario => usuario.Id);
+
+        builder
+            .Property(usuario => usuario.Nome)
+            .HasColumnType("varchar(100)")
+            .IsRequired();
+    }
+}
