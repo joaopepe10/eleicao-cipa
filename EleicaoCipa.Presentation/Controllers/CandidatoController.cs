60fdf8dbaf161a85b3ba71a84047223ce87826af
Joao Barcante <joao.barcante@somalogistica.com.br>
2024-03-15 09:42:58 -0300

    Adciona controllers

---
+++ EleicaoCipa.Presentation/Controllers/CandidatoController.cs
@@ -0,0 +1,44 @@
+﻿using EleicaoCipa.Aplicacao.DTO.Dto.CandidatoDto.RequestDto;
+using EleicaoCipa.Aplicacao.DTO.Dto.CandidatoDto.ResponseDto;
+using EleicaoCipa.Aplicacao.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EleicaoCipa.Apresentacao.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class CandidatoController : ControllerBase
+{
+    private readonly ApplicationServiceCandidato _service;
+    public CandidatoController(ApplicationServiceCandidato service)
+    {
+        _service = service;
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+        try
+        {
+            var responseDto = _service.GetById(id);
+            return Ok(responseDto);
+        }
+        catch (ArgumentException e)
+        {
+            return NotFound(e.Message);
+        }
+    }
+
+    [HttpPut("{id}/altera_discurso")]
+    public IActionResult PutDiscurso(int id, UpdateDiscursoDto dto)
+    {
+        if (dto is null || dto.Discurso.Equals(string.Empty))
+            return NotFound("Não é possivel fazer alguma alteração com o discurso nulo ou vazio.");
+        return Ok(_service.PutDiscurso(id, dto));
+    }
+
+    [HttpGet]
+    public IEnumerable<ReadCandidatoDto> GetAll()
+    {
+        return _service.GetAll();
+    }
+}
---
+++ EleicaoCipa.Presentation/Controllers/UsuarioController.cs
@@ -0,0 +1,44 @@
+﻿using EleicaoCipa.Aplicacao.DTO.Dto.UsuarioDto.RequestDto;
+using EleicaoCipa.Aplicacao.DTO.Dto.UsuarioDto.ResponseDto;
+using EleicaoCipa.Aplicacao.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EleicaoCipa.Apresentacao.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class UsuarioController : ControllerBase
+{
+    private readonly ApplicationServiceUsuario _service;
+    public UsuarioController(ApplicationServiceUsuario service)
+    {
+        _service = service;
+    }
+
+    [HttpPost]
+    public IActionResult Post([FromBody] CreateUsuarioDto dto)
+    {
+        if (dto is null) return NotFound("Dado para criação de usuário inválidos.");
+        var responseDto = _service.Post(dto);
+        return CreatedAtAction(nameof(GetById), new { responseDto.Id }, responseDto);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+        var dto = _service.GetById(id);
+        return Ok(dto);
+    }
+
+    [HttpGet]
+    public IEnumerable<ReadUsuarioDto> GetAll()
+    {
+        return _service.GetAll();
+    }
+
+    [HttpGet("candidatos")]
+    public IEnumerable<ReadAllCandidatosDto> GetAllCandidatos()
+    {
+        var candidatos = _service.GetAllCandidato();
+        return candidatos;
+    }
+}
