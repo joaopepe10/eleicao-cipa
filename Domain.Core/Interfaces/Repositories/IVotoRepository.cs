37257df41a4f0967e0f5140947e89b6b7adc48ae
Joao Barcante <joao.barcante@somalogistica.com.br>
2024-03-15 13:27:51 -0300

    Altera nome da class de IVotoRepository

--- Domain.Core/Interfaces/Repositories/IVotoRepository.cs
+++ Domain.Core/Interfaces/Repositories/IVotoRepository.cs
@@ -1,6 +1,6 @@
 ﻿using EleicaoCipa.Dominio.Model;
 
 namespace EleicaoCipa.Domain.Nucleo.Interfaces.Repositories;
-public interface IRepositoryVoto : IRepositoryBase<Voto>
+public interface IVotoRepository : IRepositoryBase<Voto>
 {
 }
